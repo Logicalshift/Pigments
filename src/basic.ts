b3a2d2a8ebc751a438a18160960be709cf251a62
Andrew Hunter <andrew@logicalshift.co.uk>
2015-09-18 16:43:38 +0100

    Start some work on this colour library

---
+++ src/basic.ts
@@ -0,0 +1,63 @@
+//
+// Pigment
+//
+
+module Pigment {
+    //
+    // This is the most basic interface describing a color: a red green and blue triplet.
+    //
+    // Values start from (0,0,0), which represents black. (1,1,1) represents the brightest
+    // a pixel can be, but this library is designed to work with higher values, representing
+    // colours brighter than those than a screen can display. These values use a linear
+    // encoding (so that 0.5 is half as bright as 1.0)
+    //
+    // RGB colours such as this represent an emissive color: ie, that emanating from a light
+    // source. Pixels on a computer monitor are effectively tiny lights, so this is a good
+    // way to represent the light emitted from a pixel.
+    //
+    // This is also a good way to represent the colour of a light source (such as a bulb or
+    // the sun).
+    //
+    export interface EmissiveColor {
+        red: number;
+        green: number;
+        blue: number;
+    };
+
+    export class EmmisiveColor implements EmmisiveColor {
+
+    };
+
+    //
+    // This represents the colour of a physical object
+    //
+    // Colour in physical objects works by absorbing light; a light ray (with red, green and blue components)
+    // hits an object and some portion of the various colours that make it up are absorbed as the ray is
+    // reflected in variou directions.
+    //
+    // As the values in here represent a portion of light that's reflected from an object, they have the range
+    // 0-1, where 0 represents light that is completely absorbed and 1 represents light that is completely 
+    // reflected.
+    //
+    export interface ReflectiveColor {
+        //
+        // Represents the portion of red light that is reflected by this material (0-1)
+        //
+        cyan: number;
+
+        //
+        // Represents the portion of green light that is reflected by this material (0-1)
+        //
+        magenta: number;
+
+        //
+        // Represents the portion of yellow light that is reflected by this material (0-1)
+        //
+        yellow: number;
+
+        //
+        // Represents the amount of light in general that's reflected by this material (0-1)
+        //
+        key: number;
+    };
+}
---
+++ src/gamma.ts
@@ -0,0 +1,23 @@
+//
+// Pigment
+//
+
+module Pigment {
+    //
+    // Returns the result of translating the specified value using the basic gamma function
+    //
+    // Most values we use have a linear response to make calculations easier: however, computer displays
+    // have a non-linear 'gamma' value, which reflects the logarithmic responsiveness of the human eye.
+    // This increases the amount of information that can be conveyed on the screen (ensuring that colours
+    // with different RGB values look different in reality)
+    //
+    // It turns out that floating point values, which we use to represent colour values, already have this
+    // non-linearity of precision built in, so there's no need to represent light in a non-linear fashion
+    // until it's time to actually generate an RGB hex value. This function is used to convert from these
+    // linear values to the non-linear values used by the tool.
+    //
+    // Most computer displays are calibrated with a gamma factor of 1.2.
+    //
+    function gamma(value: number, factor: number) {
+    }
+}
